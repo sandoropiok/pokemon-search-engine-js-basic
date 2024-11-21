e8660f2253f842c861396052410d5e3b89dd8fb2
Sandor Opiok <sandor.opiok@dci-student.org>
2024-11-22 00:33:22 +0100

    js basic changed and fixed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ async function fetchPokemonData(query) {
 
 // Function to display the Pokémon card
 function displayPokemonCard(pokemon) {
-  const { name, sprites, stats, abilities, weight, length } = pokemon;
+  const { name, sprites, stats, abilities, weight } = pokemon;
 
   // Generate stats and abilities list
   const hp = stats.find((stat) => stat.stat.name === "hp").base_stat; // HP value
@@ -40,15 +40,17 @@ function displayPokemonCard(pokemon) {
   // Create card HTML
   const cardHTML = `
     <div class="pokemon_card_content">
-    
-    <div  class="pokemon_hp">
-    <h3 class="pokemon_name">${name.charAt(0).toUpperCase() + name.slice(1)}</h3>
-    HP: ${hp}
-    </div>
-      <img src="${sprites.front_default}" alt="${name}" class="pokemon_image" />
-      <p class="pokemon_description">${description}</p>
+      <div  class="pokemon_hp">
+        <h3 class="pokemon_name">
+          ${name.charAt(0).toUpperCase() + name.slice(1)}
+        </h3>
+          HP: ${hp}
+      </div>
+        <img src="${
+          sprites.front_default
+        }" alt="${name}" class="pokemon_image" />
+        <p class="pokemon_description">${description}</p>
       <div class="pokemon_details">
-        <div class="pokemon_details">
         <h4>Stats</h4>
         <ul>${statsHTML}</ul>
         <h4>Abilities</h4>
